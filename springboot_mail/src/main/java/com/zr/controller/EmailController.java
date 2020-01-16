69b1297f5732f2145b4849a49ef492e37eeb7050
wf <wf950827@163.com>
2020-01-16 20:46:02 +0800

    123

---
+++ springboot_mail/src/main/java/com/zr/controller/EmailController.java
@@ -0,0 +1,35 @@
+package com.zr.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+
+    @Autowired
+    private JavaMailSender jms;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    @RequestMapping("sendSimpleEmail")
+    public String sendSimpleEmail() {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(from); //发送者
+            message.setTo("wf950827@163.com"); // 接收者地址
+            message.setSubject("一封简单的邮件"); // 标题
+            message.setText("使用Spring Boot发送简单邮件。"); // 内容
+            jms.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+}
